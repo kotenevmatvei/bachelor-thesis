efd05f4eab960731578c7e1e9b58437aabefbd75
Matvei Kotenev <kotenev.matvei@gmail.com>
2025-12-31 16:31:15 +0100

    rename dir

--- plot.py
+++ plot.py
@@ -16,7 +16,7 @@ for dataset in datasets[:6]:
      ax.plot(dataset)
 
 
-fig.savefig("results/probability_dens.png")
+fig.savefig("plots/probability_dens.png")
 
 times = [30, 50, 100, 400]
 
@@ -35,5 +35,5 @@ for time in times:
     ax.hist(bin_bounds[:-1], bin_bounds, weights=counts, edgecolor="red")
     ax.set_xticks([-1, -0.5, 0, 0.5, 1, 1.5])
     ax.set_xlim(-1.1, 1.5)
-    fig.savefig(f"results/hist_t{time}.png")
+    fig.savefig(f"plots/hist_t{time}.png")
 
