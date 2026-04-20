e76462f2a645dfb4c4f7bc8b0b844c56c1360ff8
Matvei Kotenev <kotenev.matvei@gmail.com>
2026-04-20 12:19:34 +0200

    setup cmake

--- main.c
+++ main.c
@@ -27,11 +27,11 @@ int main(void) {
     // simulate and write to files
     double **v_data = simulate_V_values(D, gamma, N, N_t, delta_t, r);
 
-    simple_write_double_matrix_to_file(v_data, N, N_t, "data/v_values.txt");
+    simple_write_double_matrix_to_file(v_data, N, N_t, "../data/v_values.txt");
 
     // calculate histograms and write to file
     int times[] = {30, 50, 100, 400};
-    char *hist_fname = "data/hist_data.txt";
+    char *hist_fname = "../data/hist_data.txt";
     int n_times = sizeof(times) / sizeof(int);
     for (int i = 0; i < n_times; i++) {
         int time_ = times[i];
@@ -53,7 +53,7 @@ int main(void) {
 
     LinearAxis *V_Axis = malloc((4 + n_hist_datapoints) * sizeof(double) + sizeof(int));
     fill_linear_axis(V_Axis, hist_range[0], hist_range[1], n_hist_datapoints);
-    write_simple_double_array_to_file(V_Axis->points, n_hist_datapoints, "data/theor_curves.txt",
+    write_simple_double_array_to_file(V_Axis->points, n_hist_datapoints, "../data/theor_curves.txt",
                                "w", "# V_axis\n");
 
     for (int i = 0; i < 4; i++) {
@@ -62,7 +62,7 @@ int main(void) {
 
         char *comment = malloc(COMMENT_LENGTH * sizeof(char));
         sprintf(comment, "# P values for time %.1fs\n", time_);
-        write_simple_double_array_to_file(P_vals, n_hist_datapoints, "data/theor_curves.txt",
+        write_simple_double_array_to_file(P_vals, n_hist_datapoints, "../data/theor_curves.txt",
                                    "a", comment);
 
         free(P_vals);
