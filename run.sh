188daf1b420d2cef1b95e1291be8802474ed4023
Matvei Kotenev <kotenev.matvei@gmail.com>
2026-05-17 10:03:47 +0200

    added run script

---
+++ run.sh
@@ -0,0 +1,11 @@
+echo "removing old data..."
+rm data/diffusion_trajectories.txt
+echo "building the project..."
+make
+cd cmake-build-debug/
+echo "running the simulation..."
+./single_diffusion
+cd ..
+echo "running the visualizations..."
+python plot_diffusion.py
+echo "done!"
