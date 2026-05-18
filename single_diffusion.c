5b5a6d71530e7367c9ff574708f3f464a7aeeff6
Matvei Kotenev <kotenev.matvei@gmail.com>
2026-05-18 14:16:12 +0200

    bring back trajectories to simulation

--- single_diffusion.c
+++ single_diffusion.c
@@ -13,8 +13,9 @@
 #define D 0.1
 #define N_BINS 100
 
-void diffuse_one_particle(double start, double lower_bound, double upper_bound,
-                          double delta_t, int n_t, int n_realizations, int n_bins) {
+void diffuse_save_histograms(double start, double lower_bound, double upper_bound,
+                             double delta_t, int n_t, int n_realizations, int n_bins,
+                             double d) {
 
     gsl_rng_env_setup();
     const gsl_rng_type *T = gsl_rng_default;
@@ -23,7 +24,7 @@ void diffuse_one_particle(double start, double lower_bound, double upper_bound,
     double *coordinates = alloc_fill_double_array(start, n_realizations);
     for (int j = 0; j < n_t; j++) {
         for (int j = 0; j < n_realizations; j++) {
-            double coordinate = simple_diffuse(coordinates[j], D, delta_t, r);
+            double coordinate = simple_diffuse(coordinates[j], d, delta_t, r);
             coordinates[j] = reflective_boundary(coordinate, lower_bound, upper_bound);
         }
         double *bin_bounds = malloc((n_bins + 1) * sizeof(double));
@@ -39,7 +40,32 @@ void diffuse_one_particle(double start, double lower_bound, double upper_bound,
     free(coordinates);
 }
 
+void diffuse_save_trajectories(double start, double lower_bound, double upper_bound,
+                               double delta_t, int n_t, int n_realizations, double d) {
+
+    gsl_rng_env_setup();
+    const gsl_rng_type *T = gsl_rng_default;
+    gsl_rng *r = gsl_rng_alloc(T);
+
+    for (int j = 0; j < n_realizations; j++) {
+
+        double coordinate = start;
+        double *trajectory = malloc(n_t * sizeof(double));
+
+        for (int j = 0; j < n_t; j++) {
+            coordinate = simple_diffuse(coordinate, d, delta_t, r);
+            coordinate = reflective_boundary(coordinate, lower_bound, upper_bound);
+            trajectory[j] = coordinate;
+        }
+
+        write_double_array_to_file(trajectory, n_t, "../data/diffusion_trajectories.txt",
+                                   "a", "");
+        free(trajectory);
+    }
+}
+
 int main() {
-    diffuse_one_particle(START, LOWER_BOUND, UPPER_BOUND, DELTA_T, N_T, N_REALIZATIONS,
-                         N_BINS);
+    diffuse_save_trajectories(START, LOWER_BOUND, UPPER_BOUND, DELTA_T, 1000, 10, D);
+    diffuse_save_histograms(START, LOWER_BOUND, UPPER_BOUND, DELTA_T, N_T, N_REALIZATIONS,
+                            N_BINS, D);
 }
