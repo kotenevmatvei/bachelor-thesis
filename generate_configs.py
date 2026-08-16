913823c3536f2c689f7e733b3d910ae8599723ab
Matvei Kotenev <kotenev.matvei@gmail.com>
2026-08-16 23:35:06 +0200

    generate real slurm & configs

--- generate_configs.py
+++ generate_configs.py
@@ -1,7 +1,16 @@
-q3_c = [1, 1.5, 1.7, 1.9, 1.95, 1.97, 1.99, 2, 2.01, 2.03, 2.05, 2.1, 2.3, 2.5, 3]
+q3_c = [1, 1.5, 1.7, 1.9, 1.95, 1.97, 1.99, 2, 2.01, 2.03, 2.05, 2.1, 2.3, 2.5, 3, 4]
+q2_c = [4, 5, 5.1, 5.2, 5.25, 5.3, 5.33, 5.333, 5.34, 5.345, 5.4, 5.5, 5.6, 5.8, 6, 7]
+
+print(f"q3: {len(q3_c)}, q2: {len(q2_c)}")
 
 for c in q3_c:
     c_str = str(c)
-    content = f"type s3d_correct\ndelta_t 0.0001\nstart 0\nlower_bound -1\nupper_bound 1\nd 1\nn_t 10000\nn_realizations 10000\nn_bins 100\nc {c_str}\nq 3\nrs 0\nframe_timestep 100"
+    content = f"type s3d_correct\ndelta_t 0.0001\nstart 0\nlower_bound -1\nupper_bound 1\nd 1\nn_t 100000000\nn_realizations 10000\nn_bins 100\nc {c_str}\nq 3\nrs 0\nframe_timestep 10000"
     with open(f"configs/q3_c{c}.txt", "w") as f:
+        f.write(content)
+
+for c in q2_c:
+    c_str = str(c)
+    content = f"type s3d_correct\ndelta_t 0.0001\nstart 0\nlower_bound -1\nupper_bound 1\nd 1\nn_t 100000000\nn_realizations 10000\nn_bins 100\nc {c_str}\nq 2\nrs 0\nframe_timestep 10000"
+    with open(f"configs/q2_c{c}.txt", "w") as f:
         f.write(content)
--- generate_slurm_scripts.py
+++ generate_slurm_scripts.py
@@ -1,9 +1,16 @@
+q3_c = [1, 1.5, 1.7, 1.9, 1.95, 1.97, 1.99, 2, 2.01, 2.03, 2.05, 2.1, 2.3, 2.5, 3, 4]
+q2_c = [4, 5, 5.1, 5.2, 5.25, 5.3, 5.33, 5.333, 5.34, 5.345, 5.4, 5.5, 5.6, 5.8, 6, 7]
 
-q3_c = [1, 1.5, 1.7, 1.9, 1.95, 1.97, 1.99, 2, 2.01, 2.03, 2.05, 2.1, 2.3, 2.5, 3]
+print(f"q3: {len(q3_c)}, q2: {len(q2_c)}")
 
 for c in q3_c:
     c_str = str(c)
-    content = f"#!/bin/bash\n#SBATCH --nodes=1\n#SBATCH --ntasks=1\n#SBATCH --cpus-per-task=5\n#SBATCH --time=8:00:00\n#SBATCH--output=slurm/R-%x.%j.out\n\n#SBATCH--error=slurm/R-%x.%j.err\nexport OMP_NUM_THREADS=4\nexport OMP_PLACES=cores\n\nexport OMP_PROC_BIND=spread\nbash server_run.sh q3_c{c_str}\n"
+    content = f"#!/bin/bash\n#SBATCH --nodes=1\n#SBATCH --ntasks=1\n#SBATCH --cpus-per-task=4\n#SBATCH --time=200:00:00\n#SBATCH--output=slurm/R-%x.%j.out\n\n#SBATCH--error=slurm/R-%x.%j.err\nexport OMP_NUM_THREADS=4\nexport OMP_PLACES=cores\n\nexport OMP_PROC_BIND=spread\nbash server_run.sh q3_c{c_str}\n"
     with open(f"slurm/q3_c{c}.slurm", "w") as f:
         f.write(content)
 
+for c in q2_c:
+    c_str = str(c)
+    content = f"#!/bin/bash\n#SBATCH --nodes=1\n#SBATCH --ntasks=1\n#SBATCH --cpus-per-task=4\n#SBATCH --time=200:00:00\n#SBATCH--output=slurm/R-%x.%j.out\n\n#SBATCH--error=slurm/R-%x.%j.err\nexport OMP_NUM_THREADS=4\nexport OMP_PLACES=cores\n\nexport OMP_PROC_BIND=spread\nbash server_run.sh q2_c{c_str}\n"
+    with open(f"slurm/q2_c{c}.slurm", "w") as f:
+        f.write(content)
